d1a725a1f2a71383bffefbd044a3e67f0db169d0
viholv7 <61299149+viholv7@users.noreply.github.com>
2020-04-05 16:41:29 -0300

    teste3

--- com/Breno/test/test_operacoes.py
+++ com/Breno/test/test_operacoes.py
@@ -6,5 +6,5 @@ class TestOperacoes(TestCase):
     def setUp(self):
         self.operacoes = Operacoes()
     
-    def test_subtracao(self):
-        self.assertEqual(self.operacoes.subtracao([10, 5]), 9, "Deveria ser 5")
+    def test_soma(self):
+        self.assertEqual(self.operacoes.soma([2, 2]), 4, "retorna 4")
