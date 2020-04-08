f61f2090338bd1b8afc4564f298d014fa50755be
viholv7 <victoriagoliveira7@gmail.com>
2020-04-08 19:50:17 -0300

    atualização 4

--- com/Breno/test/test_operacoes.py
+++ com/Breno/test/test_operacoes.py
@@ -7,4 +7,4 @@ class TestOperacoes(TestCase):
         self.operacoes = Operacoes()
     
     def test_soma(self):
-        self.assertEqual(self.operacoes.soma([2, 2]), 4, "retorna 4")
+        self.assertEqual(self.operacoes.soma([3, 2]), 4, "retorna 5")
